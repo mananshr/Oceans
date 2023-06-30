518da322bab1e72be38ebad0e2c7a88305250b4c
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-06-30 05:40:02 +0530

    Oceans start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+</script>
+<h1 class="text-3xl font-bold underline">Oceans</h1> <br/>
+<h2>RED OCEAN VS BLUE OCEAN</h2><br/>
+
+<p>
+Red oceans are all the industries in existence today – the known market space, where industry boundaries are defined and companies try to outperform their rivals to grab a greater share of the existing market. Cutthroat competition turns the ocean bloody red. Hence, the term ‘red’ oceans.
+Blue oceans denote all the industries not in existence today – the unknown market space, unexplored and untainted by competition. Like the ‘blue’ ocean, it is vast, deep and powerful – in terms of opportunity and profitable growth.
+The chart below summarizes the distinct characteristics of competing in red oceans (<a href="red-ocean">Red Ocean Strategy</a>) versus creating a blue ocean (<a href="/blue-ocean">Blue Ocean Strategy</a>).
+</p>
+
+<style lang="postcss">
+:global(html) {
+  background-color: theme(colors.blue.400);
+}
+</style>
