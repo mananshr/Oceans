81c8bb1883b827ccf0c0c9d59488fb085a5f68d7
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-06-30 05:40:13 +0530

    Oceans start

---
+++ src/routes/blue-ocean/+page.svelte
@@ -0,0 +1,8 @@
+<h1>WHAT IS BLUE OCEAN STRATEGY?</h1>
+
+
+<style>
+    h1 {
+        color: blue;
+    }
+</style>
---
+++ src/routes/red-ocean/+page.svelte
@@ -0,0 +1,8 @@
+<h1>What is Red Ocean Strategy and Why You Need to Shift to a Blue Ocean</h1>
+
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
