2fddb62f034a5727b2d6f1138e3e35a6ee48ba30
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-07-01 08:37:45 +0530

    Table test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,46 @@
-<script>
+9<script>
     import "../app.css";
 </script>
 <div class="p-6">
-<h1 class="text-3xl font-bold underline">Oceans</h1> <br/>
-<h2><b>OCEAN VS BLUE OCEAN</b></h2>
-<p>
-Red oceans are all the industries in existence today: the known market space, where industry boundaries are defined and companies try to outperform their rivals to grab a greater share of the existing market. Cutthroat competition turns the ocean bloody red. Hence, the term ‘red’ oceans.
-Blue oceans denote all the industries not in existence today: the unknown market space, unexplored and untainted by competition. Like the ‘blue’ ocean, it is vast, deep and powerful – in terms of opportunity and profitable growth.
-The chart below summarizes the distinct characteristics of competing in red oceans (<a href="red-ocean">Red Ocean Strategy</a>) versus creating a blue ocean (<a href="/blue-ocean">Blue Ocean Strategy</a>).
-</p>
-<p>
-🔴 Red oceans represent the fiercely competitive industries we know today, where companies fight tooth and nail for a larger slice of the existing market. It's a battlefield that turns the waters bloody red. 💉
-<br/>
-💙 Blue oceans, on the other hand, symbolize uncharted territory, untainted by competition. They hold vast opportunities for growth and innovation, like a deep and powerful ocean waiting to be explored. 🌊
-</p>
-<br />
-<h2 class="text-2xl">In a nutshell:</h2>
-<b>🔴 Red Ocean Strategy:</b>
-<ul style="list-style-type:disc;">
-<li>Cut throat competition 🗡️</li>
-<li>Defined industry boundaries 📏</li>
-<li>Fighting for market share 🥊</li>
-<li>Rivalry in bloodied waters 💔</li>
-</ul>
+    <h1 class="text-3xl font-bold underline">Oceans</h1> <br/>
+    <h2><b>OCEAN VS BLUE OCEAN</b></h2>
+    <p>
+        Red oceans are all the industries in existence today: the known market space, where industry boundaries are defined and companies try to outperform their rivals to grab a greater share of the existing market. Cutthroat competition turns the ocean bloody red. Hence, the term ‘red’ oceans.
+        Blue oceans denote all the industries not in existence today: the unknown market space, unexplored and untainted by competition. Like the ‘blue’ ocean, it is vast, deep and powerful – in terms of opportunity and profitable growth.
+        The chart below summarizes the distinct characteristics of competing in red oceans (<a href="red-ocean">Red Ocean Strategy</a>) versus creating a blue ocean (<a href="/blue-ocean">Blue Ocean Strategy</a>).
+    </p>
+    
+    <p>
+        🔴 Red oceans represent the fiercely competitive industries we know today, where companies fight tooth and nail for a larger slice of the existing market. It's a battlefield that turns the waters bloody red. 💉
+        <br/>
+        💙 Blue oceans, on the other hand, symbolize uncharted territory, untainted by competition. They hold vast opportunities for growth and innovation, like a deep and powerful ocean waiting to be explored. 🌊
+    </p>
+    <br />
+    <h2 class="text-2xl">In a nutshell:</h2>
+    <b>🔴 Red Ocean Strategy:</b>
+    <ul style="list-style-type:disc;">
+        <li>Cut throat competition 🗡️</li>
+        <li>Defined industry boundaries 📏</li>
+        <li>Fighting for market share 🥊</li>
+        <li>Rivalry in bloodied waters 💔</li>
+    </ul>
 
-<b>💙 Blue Ocean Strategy:</b>
-<ul style="list-style-type:disc;">
-<li>Untapped market space 🗺️</li>
-<li>Limitless potential 🌟</li>
-<li>Breaking free from competition ✨</li>
-<li>Creating your own market! 🚀</li>
-</ul>
-<br />
-<b>Key Distinctions between Red Ocean Strategy and Blue Ocean Strategy</b>
-<br />
-To thrive in the business landscape, red ocean strategists primarily focus on gaining competitive advantages by analyzing their rivals' approaches and striving to outperform them. They perceive the market as a finite space, where capturing a larger share entails a zero-sum game, meaning one company's gain is achieved at the expense of another. Their emphasis lies in dividing the existing market (red ocean) that is facing limited growth. This mindset leads companies to classify industries as attractive or unattractive, determining whether they should enter or not based on these assessments.
-On the other hand, blue ocean strategists recognize that market boundaries are merely conceptual and should not restrict their thinking. They perceive significant untapped demand outside the confines of existing market structures. The central challenge lies in creating this demand, requiring a shift in focus from supply to demand and from competition to generating innovative value that unlocks new demand. This is accomplished by simultaneously pursuing differentiation and cost efficiency.
-Within the framework of blue ocean strategy, industries themselves are not inherently attractive or unattractive since companies can actively modify the level of industry attractiveness through deliberate efforts. By breaking the traditional value-cost trade-off, the market structure undergoes transformation, and new rules are established. Consequently, competition in the previous framework becomes irrelevant. By expanding the demand side of the economy, this strategy generates new wealth. As a result, firms can engage in a non-zero-sum game, presenting significant potential for high returns.
-In summary, red ocean strategy focuses on outperforming competitors within existing market boundaries, while blue ocean strategy seeks to create new demand and redefine market boundaries to gain a competitive advantage.
-<br /><br />
-<table>
+    <b>💙 Blue Ocean Strategy:</b>
+    <ul style="list-style-type:disc;">
+        <li>Untapped market space 🗺️</li>
+        <li>Limitless potential 🌟</li>
+        <li>Breaking free from competition ✨</li>
+        <li>Creating your own market! 🚀</li>
+    </ul>
+    <br />
+    <b>Key Distinctions between Red Ocean Strategy and Blue Ocean Strategy</b>
+    <br />
+        To thrive in the business landscape, red ocean strategists primarily focus on gaining competitive advantages by analyzing their rivals' approaches and striving to outperform them. They perceive the market as a finite space, where capturing a larger share entails a zero-sum game, meaning one company's gain is achieved at the expense of another. Their emphasis lies in dividing the existing market (red ocean) that is facing limited growth. This mindset leads companies to classify industries as attractive or unattractive, determining whether they should enter or not based on these assessments.
+        On the other hand, blue ocean strategists recognize that market boundaries are merely conceptual and should not restrict their thinking. They perceive significant untapped demand outside the confines of existing market structures. The central challenge lies in creating this demand, requiring a shift in focus from supply to demand and from competition to generating innovative value that unlocks new demand. This is accomplished by simultaneously pursuing differentiation and cost efficiency.
+        Within the framework of blue ocean strategy, industries themselves are not inherently attractive or unattractive since companies can actively modify the level of industry attractiveness through deliberate efforts. By breaking the traditional value-cost trade-off, the market structure undergoes transformation, and new rules are established. Consequently, competition in the previous framework becomes irrelevant. By expanding the demand side of the economy, this strategy generates new wealth. As a result, firms can engage in a non-zero-sum game, presenting significant potential for high returns.
+        In summary, red ocean strategy focuses on outperforming competitors within existing market boundaries, while blue ocean strategy seeks to create new demand and redefine market boundaries to gain a competitive advantage.
+    <br />
+    <br />
     <table>
         <tr>
             <th class="red">RED OCEAN STRATEGY</th>
@@ -67,30 +68,31 @@ In summary, red ocean strategy focuses on outperforming competitors within exist
         </tr>
     </table>
 </div>
+
 <style lang="postcss">
-:global(html) {
-  background-color: theme(colors.blue.200);
-}
-table {
-    border-collapse: collapse;
-    width: 100%;
-}
-th, td {
-    text-align: left;
-    padding: 8px;
-}
-th.red {
-    background-color: #ffb2b2;
-    color: red;
-}
-th.blue {
-    background-color: #9dccff;
-    color: blue;
-}
-td.red {
-    background-color: #ffcccc;
-}
-td.blue {
-    background-color: #cce6ff;
-}
+    :global(html) {
+        background-color: theme(colors.blue.200);
+    }
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th, td {
+        text-align: left;
+        padding: 8px;
+    }
+    th.red {
+        background-color: #ffb2b2;
+        color: red;
+    }
+    th.blue {
+        background-color: #9dccff;
+        color: blue;
+    }
+    td.red {
+        background-color: #ffcccc;
+    }
+    td.blue {
+        background-color: #cce6ff;
+    }
 </style>
