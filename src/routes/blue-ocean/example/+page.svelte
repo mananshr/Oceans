a3345c9dc61950986dd1a520396d4eb3a880fd16
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-07-01 20:04:00 +0530

    examples intro

---
+++ src/routes/blue-ocean/example/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+    import "../../../app.css";
+</script>
+<div class="p-6">
+    <h1 class="mb-8">Some examples of blue ocean strategies</h1>
+    <p>
+        Blue Ocean Strategy is a concept that encourages businesses to seek untapped market spaces, where competition is
+        minimal or nonexistent. It promotes innovative thinking and value creation, allowing companies to escape the
+        cutthroat competition of existing markets and create their own uncontested market space. By focusing on
+        differentiation and low cost simultaneously, blue ocean strategy enables organizations to achieve both high
+        customer value and sustainable profitability.
+    </p>
+    <p><br />In this article, we will explore some compelling examples of companies that have successfully applied blue
+        ocean strategy to revolutionize their industries and unlock new opportunities. These examples demonstrate the
+        power of thinking outside the box and challenging industry norms to create uncontested market spaces. Let's dive
+        in and discover how these organizations carved their own blue oceans:
+    </p>
+    <p><br />Cirque du Soleil: By blending elements of traditional circus acts with theater and high-quality production,
+        Cirque du Soleil created a new form of entertainment that appealed to a broader audience. It shifted the focus
+        from animal acts and star performers to artistic performances, captivating audiences worldwide. Cirque du Soleil
+        redefined the boundaries of the circus industry, leading to increased profitability and reduced competition.
+    </p>
+    <p><br />Yellow Tail: In the highly competitive wine industry, Yellow Tail, an Australian wine brand, adopted a blue
+        ocean strategy by targeting a new customer segment—novice wine drinkers in the United States. They simplified
+        the wine selection process, used innovative marketing techniques, and offered a casual and approachable brand
+        image. Yellow Tail disrupted the industry dominated by traditional and complex wine brands, achieving rapid
+        growth and market share expansion.
+    </p>
+    <p><br />Nintendo Wii: While the gaming market was primarily focused on catering to hardcore gamers, Nintendo
+        introduced the Wii console, targeting casual gamers and non-gamers. By utilizing motion-sensing controllers and
+        intuitive gameplay, Nintendo revolutionized the gaming experience. The Wii appealed to a wider demographic,
+        including families and elderly individuals, and became a massive success, creating a blue ocean in the gaming
+        industry.
+    </p>
+    <p><br />Southwest Airlines: By redefining the value curve in the airline industry, Southwest Airlines pioneered the
+        low-cost carrier model. They eliminated unnecessary services, implemented quick turnarounds, and offered
+        affordable fares. Southwest Airlines attracted a new customer base seeking no-frills travel, thus creating a
+        blue ocean separate from traditional full-service airlines. Their innovative strategy led to sustained
+        profitability and growth.
+    </p>
+    <p><br />Netflix: Originally a DVD rental-by-mail service, Netflix disrupted the home entertainment industry by
+        introducing a subscription-based streaming platform. By leveraging technology and data-driven personalization,
+        they offered a vast library of movies and TV shows, accessible anytime, anywhere. Netflix redefined how people
+        consume content, rendering traditional video rental stores obsolete and establishing a dominant position in the
+        streaming industry.
+    </p>
+    <p><br />Tesla: The electric vehicle (EV) market was niche and limited to environmentally-conscious consumers until
+        Tesla Motors entered the scene. Tesla disrupted the industry by creating high-performance electric cars that
+        were both stylish and technologically advanced. They built a charging infrastructure, focused on direct sales,
+        and positioned themselves as a luxury brand. Tesla's innovative approach and dedication to sustainability
+        propelled them into a blue ocean, reshaping the automotive industry.
+    </p>
+    <p><br />Airbnb: By connecting homeowners with travelers seeking unique accommodations, Airbnb created a
+        peer-to-peer online marketplace for lodging. They disrupted the traditional hotel industry by offering diverse
+        and cost-effective accommodation options, personalized experiences, and a sense of belonging. Airbnb tapped into
+        an untapped market space and transformed the way people travel and book accommodations.
+    </p>
+    <p><br />These examples demonstrate the power of blue ocean strategy in generating breakthrough innovations,
+        creating new demand, and reshaping industries. By challenging conventional wisdom and finding unique value
+        propositions, companies can successfully navigate uncharted waters and achieve long-term success.
+</div>
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.200);
+    }
+
+    div {
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+
+    h1 {
+        color: blue;
+    }
+</style>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,14 @@
+  
+<script>
+  let count = 0;
+  
+    function incrementCount() {
+      // event handler code goes here
+      count+=1;
+    }  
+  </script>
+  
+  <button class="m-6" on:click={incrementCount}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+  </button>
